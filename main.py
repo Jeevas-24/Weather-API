c3ceaebf11ee5b551b5bad617ff0d024a934ec5b
jeeva <jeevasathappan24@gmail.com>
2024-10-30 16:33:47 +0530

    Show station table on homepge

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 import pandas as pd
 import os
+
 app = Flask(__name__)
+
+station_details = pd.read_csv('data_small/stations.txt', skiprows=17)
+station_details = station_details[['STAID', 'STANAME                                 ']]
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', data=station_details.to_html())
+
 
 @app.route('/api/v1/<station>/<date>/')
 def about(station, date):
-    file_name = os.path.abspath('data_small/TG_STAID' + str(station).zfill(6) + '.txt')
+    file_name = os.path.abspath(
+        'data_small/TG_STAID' + str(station).zfill(6) + '.txt')
     df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {'station': station,
             'date': date,
             'temperature': temperature}
