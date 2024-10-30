b81a3a47563cfd563288e037f2ab0632225ef3c9
jeeva <jeevasathappan24@gmail.com>
2024-10-30 18:01:16 +0530

    Final commit with requirements.txt and All data for particular year and particular station.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 app = Flask(__name__)
 
 station_details = pd.read_csv('data_small/stations.txt', skiprows=17)
-station_details = station_details[['STAID', 'STANAME                                 ']]
+station_details = station_details[
+    ['STAID', 'STANAME                                 ']]
 
 
 @app.route('/')
@@ -22,6 +23,24 @@ def about(station, date):
     return {'station': station,
             'date': date,
             'temperature': temperature}
+
+
+@app.route('/api/v1/<station>/')
+def all_data(station):
+    file_name = os.path.abspath(
+        'data_small/TG_STAID' + str(station).zfill(6) + '.txt')
+    df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient='records')
+
+
+@app.route('/api/v1/yearly/<station>/<year>/')
+def yearly(station, year):
+    file_name = os.path.abspath(
+        'data_small/TG_STAID' + str(station).zfill(6) + '.txt')
+    df = pd.read_csv(file_name, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))]
+    return result.to_dict(orient='records')
 
 
 if __name__ == '__main__':
